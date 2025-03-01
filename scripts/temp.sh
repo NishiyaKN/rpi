3c2df1d48514583ec96ec7c9865f2f38d629fc8c
NishiyaKN <udonge@proton.me>
2025-02-28 21:33:26 -0300

    update

--- scripts/temp.sh
+++ scripts/temp.sh
@@ -1,5 +1,3 @@
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
-echo "$(date) @ $(hostname)"
-echo "-------------------------------------------"
 echo "GPU => $(/usr/bin/vcgencmd measure_temp | cut -d '=' -f 2)"
 echo "CPU => $((cpu/1000))'C"
