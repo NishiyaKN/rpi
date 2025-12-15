38991df0b37e0c0d6b98ab9891510c562d415e4e
name <email>
2025-12-15 00:09:56 +0000

    Backup: 2025-12-15 00:09:56

--- scripts/baka.sh
+++ scripts/baka.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-cd $HOME/rpi/
-cp $HOME/.bash_aliases .
-cp $HOME/.vimrc .
-cp -r $HOME/docker .
+cp $HOME/.bash_aliases $HOME/rpi
+cp $HOME/.vimrc . $HOME/rpi
+
+rsync -av --delete \
+    --exclude '.env' \
+    --exclude '*.db' \
+    --exclude '*.db-journal' \
+    --exclude '.git' \
+    "$HOME/docker" "$HOME/rpi"
+
 git add .
-git commit -m 'update files'
+git commit -m "Backup: $(date +'%Y-%m-%d %H:%M:%S')"
 git push origin main
-
-
